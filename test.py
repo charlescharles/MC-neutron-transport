b7dfe5b8d6bfba2e82b9b127e18579b8510c307a
Charles <cguo@princeton.edu>
2013-05-06 23:20:15 -0400

    removed redundant stuff

--- test.py
+++
@@ -1,6 +0,0 @@
-def test(**kwargs):
-    if 'den' in kwargs: print('den:{}'.format(kwargs['den']))
-    if kwargs['mfp']: print('mfp:{}'.format(kwargs['mfp']))
-
-if __name__ == "__main__":
-    test(den=12.3, mfp=2.3)
